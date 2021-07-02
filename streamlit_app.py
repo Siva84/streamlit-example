69576e662a5372d8cb5f5adadb87db5c4a1b94a9
Siva84 <65079339+Siva84@users.noreply.github.com>
2021-07-02 20:42:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,3 @@
-# Sidebar Column
-st.sidebar.title('Sidebar Widgets')
-#radio button 
-rating = st.sidebar.radio('Are You Happy with the Example',('Yes','No','Not Sure'))
-if rating == 'Yes':
-    st.sidebar.success('Thank You for Selecting Yes')
-elif rating =='No':
-    st.sidebar.info('Thank You for Selecting No')
-elif rating =='Not Sure':
-    st.sidebar.info('Thank You for Selecting Not sure')
-#selectbox
-rating = st.sidebar.selectbox("How much would you rate this App? ",
-                     ['5 Stars', '4 Stars', '3 Stars','2 Stars','1 Star'])
-st.sidebar.success(rating)
-st.sidebar.write('Find Square of a Number')
-#slider
-get_number = st.sidebar.slider("Select a Number", 1, 10)
-st.sidebar.write('Square of Number',get_number, 'is', get_number*get_number)
+st.table(df.groupby("room_type").price.mean().reset_index()\
+.round(2).sort_values("price", ascending=False)\
+.assign(avg_price=lambda x: x.pop("price").apply(lambda y: "%.2f" % y)))
